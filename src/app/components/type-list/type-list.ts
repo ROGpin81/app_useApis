28f8d981cf81056c8ea87a7538d33f153e837c30
ROGpin81 <roger.pinedab@gmail.com>
2026-03-25 00:50:39 -0600

    Create estructure
    Angular project

---
+++ src/app/components/type-list/type-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-type-list',
+  imports: [],
+  templateUrl: './type-list.html',
+  styleUrl: './type-list.css',
+})
+export class TypeList {
+
+}
